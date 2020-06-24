0158e0d7c55862a42cbdbebaf2f384f14f706525
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-23 22:34:34 -0300

    Controller Get database

---
+++ WebAPI.Shippers/Controllers/TransportadorasController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Shippers.Models;
+
+namespace WebAPI.Shippers.Controllers
+{
+    public class TransportadorasController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Transportadora> Get()
+        {
+            var db = new NorthwindEntities();
+            return db.Transportadoras.ToList();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
