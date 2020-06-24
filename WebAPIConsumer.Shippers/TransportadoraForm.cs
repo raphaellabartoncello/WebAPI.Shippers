b9e244ab76e1eb082e579f3a1bbba1f12f279386
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-24 18:57:04 -0300

    Criar formulário de exibição do resultado

---
+++ WebAPIConsumer.Shippers/TransportadoraForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebAPIConsumer.Shippers
+{
+    public partial class TransportadoraForm : Form
+    {
+        public TransportadoraForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
